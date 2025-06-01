c243bc9dec5a2ea3afa87440dae2dd0470fc5726
burhan kach <burhanstud@gmail.com>
2025-06-01 02:02:57 -0700

    Added movie names to movie world file

--- studProfile/movie-page/name.js
+++ studProfile/movie-page/name.js
@@ -3,6 +3,13 @@
 const movieSets = {
     // ............2025...................
     "new": [
+        586369, // 28 Degree Celsius
+        1421019, // Devil's Double Next Level
+        1126166, // Flight Risk
+        985667, // Gajaana
+        972533, // Last Breath
+        1261125, // Thudarum
+        1189767, //Retro 
         552524, //Lilo and Stitch
         1262669, //Kapkapiii
         1232546, //Until Dawn
@@ -23,17 +30,16 @@ const movieSets = {
         1272121, //Maranamass
         1472031, //Chaurya Paatham 
         965087, //Phule
-        1189767, //Retro 
         1009640, //Valiant One 
         986056, //Thunderbolts 
         1227128, //Raid 2 
         1060046, //HIT – The third Case 
-        1259024, //Good Bad Ugly 
-        1306845, //Jaat 
-        1467506, //Costao 
     ],
     // ............2025...................
     "2025": [
+        1259024, //Good Bad Ugly 
+        1306845, //Jaat 
+        1467506, //Costao 
         1045938, // G20  
         1251551, // Pravinkoodu Shappu  
         1407861, // The Bayou  
@@ -53,6 +59,58 @@ const movieSets = {
         1020414, // Peter Pan’s Neverland Nightmare  
         1464870, // The Dupatta Killer  
         1195506, // Novocaine 
+        1244068, // Ponman  
+        777443, // The Electric State  
+        949380, // Vanangaan  
+        1449590, // Daitya  
+        1381727, // Operation Raavan  
+        1211472, // September 5  
+        610219, // The Fire Inside  
+        1084031, // Chhorii 2  
+        1424217, // Court state vs a nobody  
+        1233575, // Black Bag  
+        1444255, // 14 Days Girlfriend Intlo  
+        950387, // A Minecraft Movie  
+        1120762, // Robinhood  
+        617120, // Test  
+        1034541, // Terrifier 3  
+        1222064, // River of Blood  
+        1461258, // Kaafir  
+        926027, // Vasco Da Gama  
+        1265623, // Banger  
+        1147546, // The Diplomat  
+        1305009, // Kotee  
+        1243508, // Andhakara  
+        1467840, // The Durga Mehta Case  
+        1260435, // Tantra  
+        1229730, // Carjackers  
+        604685, // Den of Thieves 2 Pantera  
+        627336, // L2 Empuraan  
+        1455239, // Jija Sala Jija  
+        1455240, // Kiss Kiss Kissik  
+        447273, // Snow_White
+        1145725, // Tyler Perry’s Duplicity  
+        1428777, // Tumko Meri Kasam  
+        1437446, // The Twister Caught in the Storm  
+        1321718, // Purushothamudu
+        1443168, // Pintu Ki Pappi
+        1281091, // Officer on Duty  
+        966900, // Indrani Epic-1 Dharam vs Karam  
+        324544, // In the Lost Lands  
+        1297763, // Batman Ninja vs Yakuza League  
+        1452776, // Baida  
+        1173019, // Assassin's Guild  
+        1153230, // Khadaan 
+        1449539, // RIWAJ  
+        1252542, // Swag
+        1196943, // Chhaava
+        459003, // Mavka The Forest Song  
+        1062715, // Sabdham 
+        786345, // Viduthalai Part 1  
+        607141, // Be Happy
+        1447985, // Aachari Baa
+        1043905, // Dirty Angels
+        1214509, // In a Violent Nature
     ],
 
     // ............2024...................
